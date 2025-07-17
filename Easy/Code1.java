e56a7107f785e1fd38297fcf43ed697a7c97fc5c
Bhoomi <bhoomishekapure@gmail.com>
2025-07-17 21:32:49 +0530

    Update Code1.java

--- Easy/Code1.java
+++ Easy/Code1.java
@@ -1,36 +1,58 @@
 /*Task
-In this challenge, you must read  integers from stdin and then print them to stdout. 
-Each integer must be printed on a new line. 
+Given an integer, , perform the following conditional actions:
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
 
 Input Format:
-There are  lines of input, and each line contains a single integer.
+A single line containing a positive integer
 
-Sample Input:
-42
-100
-125
+Output Format:
+Print Weird if the number is weird; otherwise, print Not Weird.
 
-Sample Output:
-42
-100
-125
+Sample Input 0
+3
+Sample Output 0
+Weird
+
+Sample Input 1
+24
+Sample Output 1
+Not Weird
 */
 
-
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-    
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
-       
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+if(N%2==1){
+    System.out.println("Weird");
+}
+if(N>=2&&N<=5&&N%2==0){
+    System.out.println("Not Weird");
+}
+if(N>=6&&N<=20&&N%2==0){
+    System.out.println("Weird");
+}
+if(N>20&&N%2==0){
+    System.out.println("Not Weird");
+}
+        scanner.close();
     }
 }
+
